fb32a814acb17bf0eb010b0d2078f9e10286c187
Paul Yim <paul.j.yim@gmail.com>
2021-05-24 18:03:14 +0000

    Include linker version flags for OSX build only

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -4,19 +4,24 @@ cd build
 if [[ "${target_platform}" == "osx-64" ]]; then
   # See https://github.com/AnacondaRecipes/aggregate/issues/107
   export CPPFLAGS="-mmacosx-version-min=10.9 -I${PREFIX}/include -D_FORTIFY_SOURCE=2"
-fi
 
-if [[ "${target_platform}" == "linux"* ]]; then
-  export LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
-fi
-
-cmake \
+  cmake \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DCMAKE_C_FLAGS="-mlinker-version=305" \
     -DCMAKE_CXX_FLAGS="-mlinker-version=305" \
     ..
+fi
+
+if [[ "${target_platform}" == "linux"* ]]; then
+  export LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
+  cmake \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_PREFIX_PATH=$PREFIX \
+    ..
+fi
 
 make -j${CPU_COUNT}
 make install
