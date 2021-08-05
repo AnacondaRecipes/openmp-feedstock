21f5aaee9af7b88a534307f2e20ca1b99633e46e
Kai Tietz <ktietz@anaconda.com>
2021-08-05 10:16:13 +0200

    update to version 12.0.0

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -1,24 +1,26 @@
 mkdir build
 cd build
 
-if [[ "${target_platform}" == "osx-64" ]]; then
+if [[ "${target_platform}" == osx-* ]]; then
   # See https://github.com/AnacondaRecipes/aggregate/issues/107
-  export CPPFLAGS="-mmacosx-version-min=10.9 -I${PREFIX}/include -D_FORTIFY_SOURCE=2"
+  export CPPFLAGS="-mmacosx-version-min=${MACOSX_DEPLOYMENT_TARGET} -isystem ${PREFIX}/include -D_FORTIFY_SOURCE=2"
 fi
 
 if [[ "${target_platform}" == "linux"* ]]; then
   export LDFLAGS="$LDFLAGS -static-libgcc -static-libstdc++"
 fi
 
-cmake \
+if [[ "${PKG_VERSION}" == *rc* ]]; then
+  export PKG_VERSION=${PKG_VERSION::${#PKG_VERSION}-4}
+fi
+
+cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DCMAKE_C_FLAGS="-mlinker-version=305" \
-    -DCMAKE_CXX_FLAGS="-mlinker-version=305" \
     ..
 
-make -j${CPU_COUNT}
+make -j${CPU_COUNT} VERBOSE=1
 make install
 
 rm -f $PREFIX/lib/libgomp$SHLIB_EXT
@@ -26,3 +28,7 @@ rm -f $PREFIX/lib/libgomp$SHLIB_EXT
 mkdir -p $PREFIX/lib/clang/$PKG_VERSION/include
 # Standalone libomp build doesn't put omp.h in clang's default search path
 cp $PREFIX/include/omp.h $PREFIX/lib/clang/$PKG_VERSION/include
+if [[ "$target_platform" == linux-* ]]; then
+  # move libarcher.so so that it doesn't interfere
+  mv $PREFIX/lib/libarcher.so $PREFIX/lib/libarcher.so.bak
+fi
