adaf8040bc6a34d9134e39c2194f2a9af56f4b54
Kai Tietz <ktietz@anaconda.com>
2022-09-08 09:31:01 +0200

    update 14.0.6

--- recipe/bld-llvm-openmp.bat
+++ recipe/bld-llvm-openmp.bat
@@ -1,32 +1,38 @@
+@echo on
+
+:: using subproject sources has been effectively broken in LLVM 14,
+:: so we use the entire project, but make sure we don't pick up
+:: anything in-tree other than openmp & the shared cmake folder
+robocopy llvm-project\openmp .\openmp /E
+robocopy llvm-project\cmake .\cmake /E
+:: do not check %ERRORLEVEL%! robocopy returns an exit code
+:: of 1 if one or more files were successfully copied.
+del /f /q llvm-project
+cd openmp
+
 mkdir build
 cd build
 
-if %ARCH% == 64 (
-  set "CC=clang-cl.exe"
-  set "CXX=clang-cl.exe"
-) else (
-  set "CC=clang-cl.exe -m32"
-  set "CXX=clang-cl.exe -m32"
-)
+set "CC=clang-cl.exe"
+set "CXX=clang-cl.exe"
 
 cmake -G "Ninja" ^
     -DCMAKE_BUILD_TYPE="Release" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX:PATH=%LIBRARY_PREFIX% ^
-    %SRC_DIR%
-
-if errorlevel 1 exit 1
+    ..
+if %ERRORLEVEL% neq 0 exit 1
 
 cmake --build . --target install
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 :: delete libiomp5md.dll which is incorrectly copied over in the build
 :: del /F /Q %LIBRARY_PREFIX%\bin\libiomp5md.dll
 :: del /F /Q %LIBRARY_PREFIX%\lib\libiomp5md.dll
 
 if not exist "%LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%\include\" mkdir "%LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%\include"
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
 
 :: Standalone libomp build doesn't put omp.h in clang's default search path
 cp %LIBRARY_PREFIX%\include\omp.h %LIBRARY_PREFIX%\lib\clang\%PKG_VERSION%\include
-if errorlevel 1 exit 1
+if %ERRORLEVEL% neq 0 exit 1
