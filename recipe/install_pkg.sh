ac8d44e6a89b64829707e2de6e389bf5fc043cce
xkong-anaconda <xkong@anaconda.com>
2026-01-30 22:05:52 -0600

    Fix: use BUILD_PREFIX cmake on osx where cmake not in PATH

--- recipe/install_pkg.sh
+++ recipe/install_pkg.sh
@@ -3,7 +3,13 @@ set -ex
 
 cd openmp/build
 
-cmake --install .
+# Use BUILD_PREFIX cmake if cmake not in PATH (e.g., on osx where cmake
+# is removed from output requirements to avoid circular dependency)
+if command -v cmake &> /dev/null; then
+  cmake --install .
+else
+  $BUILD_PREFIX/bin/cmake --install .
+fi
 
 rm -f $PREFIX/lib/libgomp$SHLIB_EXT
 
